ce3a4ac10e237f96f748563fe4cda1716e7764c7
Sujeeth <sujeeth31sujeeth@gmail.com>
2024-07-02 16:47:07 +0530

    working on

---
+++ lib/home_screen.dart
@@ -0,0 +1,52 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+Future<void> _showMyDialog() async {
+  return showDialog<void>(
+    context: "",
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('AlertDialog Title'),
+        content: const SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+class Home_Screen extends StatefulWidget {
+  const Home_Screen({super.key});
+
+  @override
+  State<Home_Screen> createState() => _Home_ScreenState();
+}
+
+class _Home_ScreenState extends State<Home_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+
+
+    );
+  }
+}
